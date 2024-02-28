5def149d135cea6ccfa9a9e8cff57aa2adab1a49
farisaboobacker <fariz.cmd@gmail.com>
2024-02-28 15:53:04 +0530

    first

---
+++ WebApp/Model/Product.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Model
+{
+    public enum ProductStatus
+    {
+        instock,
+        outstock,
+        notavailable
+    }
+    
+    public class Product
+    {
+        [BindProperty]
+        public Product products { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+        public string url { get; set; }
+        public ProductStatus status { get; set; }
+    }
+}
